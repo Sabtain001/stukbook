64b7b4b0b7e890f8b379450d7639c92476b4e7ec
sabtain <sabtain001@gmail.com>
2017-10-06 15:57:21 +0500

    Added posts controller, views and routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,12 @@ class Post < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :content
 
-  _auto_html_for :content do
-
+  auto_html_for :content do
     html_escape
     image
     youtube(width: "100%", height: 250, autoplay: false)
     link target: "_blank", rel: "nofollow"
     simple_format
   end
+
 end
